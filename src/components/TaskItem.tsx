d362864b060a272688a5540b93051d106023beb9
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-03-23 19:33:34 -0400

    update TaskItem component to add fields

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,48 +1,31 @@
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { styles } from '../styles/taskStyles';
 
-export function TaskItem() {
+type Task = {
+  id: string;
+  title: string;
+  priority: string;
+  time: string;
+  feeling: string;
+};
+
+export function TaskItem({ task }: { task: Task }) {
   return (
     <TouchableOpacity
-      style={styles.container}
+      style={styles.row}
       onPress={() => console.log('short press')}
-      onLongPress={() => console.log('Long press!')}
+      onLongPress={() => console.log('long press')}
     >
-      <Text style={styles.title}>This is a task!</Text>
+      <Text style={styles.titleCell}>{task.title}</Text>
+
+      <View style={styles.verticalDivider} />
+      <Text style={styles.cell}>{task.priority}</Text>
+
+      <View style={styles.verticalDivider} />
+      <Text style={styles.cell}>{task.time}</Text>
+
+      <View style={styles.verticalDivider} />
+      <Text style={styles.cell}>{task.feeling}</Text>
     </TouchableOpacity>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingVertical: 10,
-    gap: 12,
-  },
-  checkbox: {
-    width: 20,
-    height: 20,
-    borderRadius: 4,
-    borderWidth: 2,
-    borderColor: '#888',
-  },
-  checked: {
-    backgroundColor: '#888',
-  },
-  title: {
-    flex: 1,
-    fontSize: 16,
-    color: '#1a1a1a',
-  },
-  completedText: {
-    textDecorationLine: 'line-through',
-    color: '#aaa',
-  },
-  input: {
-    flex: 1,
-    fontSize: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#888',
-    paddingVertical: 2,
-  },
-});
